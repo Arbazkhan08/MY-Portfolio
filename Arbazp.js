99aa08a22db75aa614443771c3cfed04992a8c43
Arbaz khan <111728904+Arbazkhan08@users.noreply.github.com>
2024-02-22 17:16:06 +0530

    Add files via upload

---
+++ Arbazp.js
@@ -0,0 +1,34 @@
+// setTimeout(()=>{
+//     alert("Welcome to My Protfolio")
+// },2000)
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.getElementById('MenuBtn');
+    const navList = document.querySelector('ul');
+    let menuVisible = false;
+
+    menuBtn.addEventListener('click', function () {
+        if (!menuVisible) {
+            navList.classList.add('show');
+            menuVisible = true;
+        } else {
+            navList.classList.remove('show');
+            menuVisible = false;
+        }
+    });
+
+    document.addEventListener('click', function (event) {
+        const isMenuBtn = event.target.closest('#MenuBtn');
+        const isNavList = event.target.closest('ul');
+
+        if (!isMenuBtn && !isNavList) {
+            navList.classList.remove('show');
+            menuVisible = false;
+        }
+        else{
+            setTimeout(()=>{
+             navList.classList.remove('show');
+            menuVisible = false; 
+            }, 4000)
+        }
+    });
+});
